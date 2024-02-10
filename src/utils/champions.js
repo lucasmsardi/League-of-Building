1b96576ed1f7e8be78a137cf564e47ff6854a024
lucasmsardi <lucasmsardi@gmail.com>
2024-02-10 19:50:36 -0300

    added all autoattack bottom laners

--- src/utils/champions.js
+++ src/utils/champions.js
@@ -1,18 +1,164 @@
 const champions = [
   {
     id: 1,
-    name: "Caitlyn",
-    img: 'src/assets/caitlyn.png'
+    name: "Aphelios",
+    img: 'src/assets/aphelios.png',
+    bAS: 0.64,
+    ASmulti: 2.1
   },
   {
     id: 2,
-    name: "Alfredo",
-    img: 'src/assets/alfredo.png'
+    name: "Ashe",
+    img: 'src/assets/ashe.png',
+    bAS: 0.658,
+    ASmulti: 3.33
   },
   {
     id: 3,
+    name: "Caitlyn",
+    img: 'src/assets/caitlyn.png',
+    bAS: 0.681,
+    ASmulti: 4
+  },
+  {
+    id: 4,
+    name: "Draven",
+    img: 'src/assets/draven.png',
+    bAS: 0.679,
+    ASmulti: 2.7
+  },
+  {
+    id: 5,
+    name: "Ezreal",
+    img: 'src/assets/ezreal.png',
+    bAS: 0.625,
+    ASmulti: 2.5
+  },
+  {
+    id: 6,
+    name: "Jhin",
+    img: 'src/assets/jhin.png',
+    bAS: 0.625,
+    ASmulti: 3
+  },
+  {
+    id: 7,
     name: "Jinx",
-    img: 'src/assets/jinx.png'
+    img: 'src/assets/jinx.png',
+    bAS: 0.625,
+    ASmulti: 1
+  },
+  {
+    id: 8,
+    name: "Kai'sa",
+    img: 'src/assets/kaisa.png',
+    bAS: 0.644,
+    ASmulti: 1.8
+  },
+  {
+    id: 9,
+    name: "Kalista",
+    img: 'src/assets/kalista.png',
+    bAS: 0.694,
+    ASmulti: 4.5
+  },
+  {
+    id: 10,
+    name: "Kog'maw",
+    img: 'src/assets/kogmaw.png',
+    bAS: 0.665,
+    ASmulti: 2.65
+  },
+  {
+    id: 11,
+    name: "Lucian",
+    img: 'src/assets/lucian.png',
+    bAS: 0.638,
+    ASmulti: 3.3
+  },
+  {
+    id: 12,
+    name: "Miss Fortune",
+    img: 'src/assets/missfortune.png',
+    bAS: 0.656,
+    ASmulti: 3
+  },
+  {
+    id: 13,
+    name: "Nilah",
+    img: 'src/assets/nilah.png',
+    bAS: 0.697,
+    ASmulti: 3
+  },
+  {
+    id: 14,
+    name: "Samira",
+    img: 'src/assets/samira.png',
+    bAS: 0.658,
+    ASmulti: 3.3
+  },
+  {
+    id: 15,
+    name: "Senna",
+    img: 'src/assets/senna.png',
+    bAS: 0.625,
+    ASmulti: 4
+  },
+  {
+    id: 16,
+    name: "Sivir",
+    img: 'src/assets/sivir.png',
+    bAS: 0.625,
+    ASmulti: 2
+  },
+  {
+    id: 17,
+    name: "Smolder",
+    img: 'src/assets/smolder.png',
+    bAS: 0.638,
+    ASmulti: 4
+  },
+  {
+    id: 18,
+    name: "Tristana",
+    img: 'src/assets/tristana.png',
+    bAS: 0.656,
+    ASmulti: 1.5
+  },
+  {
+    id: 19,
+    name: "Twitch",
+    img: 'src/assets/twitch.png',
+    bAS: 0.679,
+    ASmulti: 3.38
+  },
+  {
+    id: 20,
+    name: "Varus",
+    img: 'src/assets/varus.png',
+    bAS: 0.658,
+    ASmulti: 4
+  },
+  {
+    id: 21,
+    name: "Vayne",
+    img: 'src/assets/vayne.png',
+    bAS: 0.658,
+    ASmulti: 3.3
+  },
+  {
+    id: 22,
+    name: "Xayah",
+    img: 'src/assets/xayah.png',
+    bAS: 0.658,
+    ASmulti: 3.9
+  },
+  {
+    id: 23,
+    name: "Zeri",
+    img: 'src/assets/zeri.png',
+    bAS: 0.658,
+    ASmulti: 2
   }
 ];
 
