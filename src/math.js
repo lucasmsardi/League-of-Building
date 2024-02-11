a221ca398791cd8fc72cc7721e6e574e23fa09ac
lucasmsardi <lucasmsardi@gmail.com>
2024-02-11 15:13:28 -0300

    tested math

--- src/math.js
+++ src/math.js
@@ -1,11 +1,12 @@
-import champions from "./utils/champions";
-import items from "./utils/items";
+import champions from "./utils/champions.js";
+import items from "./utils/items.js";
 
-const bAS = champions[0] //0.640 //champions[0]
+const bAS = champions[0].bAS //0.640 //champions[0] 0.658
 const percentage = items[0] //25 //items[0]
 
 function multiplicacao() {
     const result = bAS + (bAS * percentage)
+    console.log(result);
 }
 
-// should come up 0.658 + 0.1645 = 0.8225
+multiplicacao()
--- src/utils/items.js
+++ src/utils/items.js
@@ -2,77 +2,77 @@ const items = [
     {
         id: 1,
         name: "Blade of the Ruined King",
-        AS: 25
+        AS: 0.25
     },
     {
         id: 2,
         name: "Experimental Hexplate",
-        AS: 25
+        AS: 0.25
     },
     {
         id: 3,
         name: "Guinsoo's Rageblade",
-        AS: 25
+        AS: 0.25
     },
     {
         id: 4,
         name: "Kraken Slayer",
-        AS: 35
+        AS: 0.35
     },
     {
         id: 5,
         name: "Nashor's Tooth",
-        AS: 50
+        AS: 0.50
     },
     {
         id: 6,
         name: "Phantom Dancer",
-        AS: 65
+        AS: 0.65
     },
     {
         id: 7,
         name: "Rapid Firecannon",
-        AS: 20
+        AS: 0.20
     },
     {
         id: 8,
         name: "Runaan's Hurricane",
-        AS: 40
+        AS: 0.40
     },
     {
         id: 9,
         name: "Statikk Shiv",
-        AS: 30
+        AS: 0.30
     },
     {
         id: 10,
         name: "Stormrazor",
-        AS: 15
+        AS: 0.15
     },
     {
         id: 11,
         name: "Stridebreaker",
-        AS: 30
+        AS: 0.30
     },
     {
         id: 12,
         name: "Terminus",
-        AS: 35
+        AS: 0.35
     },
     {
         id: 13,
         name: "Trinity Force",
-        AS: 33
+        AS: 0.33
     },
     {
         id: 14,
         name: "Wit's End",
-        AS: 55
+        AS: 0.55
     },
     {
         id: 15,
         name: "Berserker's Greaves",
-        AS: 35
+        AS: 0.35
     },
 ];
 
